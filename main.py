e0459bf231cd903682699f3ea8f3c2cd8c47e773
shakiralynch1 <d00241906@student.dkit.ie>
2024-03-27 11:38:09 +0000

    :recycle: one hot encoding features gender, part_time_job, extra_activities

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import pandas as pd
-df = pd.read_csv('student-scores.csv')
-df.head()
 
+df = pd.read_csv('student-scores.csv')
+print(df.head())
+
+encoded_df = pd.get_dummies(df, columns=['gender', 'part_time_job', 'extracurricular_activities'], prefix=['gender', 'part_time_job', 'extracurricular_activities'], dtype=int, )
+
+print(encoded_df.head())
+
+
+df = pd.DataFrame(encoded_df)
+obselete_columns = ['id','first_name','last_name','email', 'career_aspiration']
+df.drop(obselete_columns, axis=1, inplace=True)
+
+print(df.head())
+
+df.to_csv('modified_student_scores.csv', index=False)
+df = pd.read_csv('modified_student_scores.csv')
+print(df.head())
 
