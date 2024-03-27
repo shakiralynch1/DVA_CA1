8cd2a29c203467511487a1df710ceb40f23f0898
shakiralynch1 <d00241906@student.dkit.ie>
2024-03-27 11:39:54 +0000

    :fire: removed features and saved to csv

--- main.py
+++ main.py
@@ -8,4 +8,11 @@ encoded_df = pd.get_dummies(df, columns=['gender', 'part_time_job', 'extracurric
 print(encoded_df.head())
 
 
+df = pd.DataFrame(encoded_df)
+obselete_columns = ['id','first_name','last_name','email', 'career_aspiration']
+df.drop(obselete_columns, axis=1, inplace=True)
+
+print(df.head())
+
+df.to_csv('modified_student_scores.csv', index=False)
 
