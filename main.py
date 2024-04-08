6d66726505ae3fc43dac3e0e080f821bd7c58e55
shakiralynch1 <d00241906@student.dkit.ie>
2024-04-08 19:57:22 +0100

    :monocle_face: Scatter plots for DV to IV correlations

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler 
 
+
+# Load the data
 df = pd.read_csv('student-scores.csv')
 print(df.head())
 
+# One-hot encode the data 
 encoded_df = pd.get_dummies(df, columns=['gender', 'part_time_job', 'extracurricular_activities'], prefix=['gender', 'part_time_job', 'extracurricular_activities'], dtype=int, )
-
 print(encoded_df.head())
 
-
+# Drop the columns that are not needed
 df = pd.DataFrame(encoded_df)
 obselete_columns = ['id','first_name','last_name','email', 'career_aspiration']
 df.drop(obselete_columns, axis=1, inplace=True)
 
 print(df.head())
 
+# Save the modified data
 df.to_csv('modified_student_scores.csv', index=False)
+
+
+
+
+
+
+
+
+
 
--- multi_variate.py
+++ multi_variate.py
@@ -89,4 +89,217 @@ plt.show()
 ##No correlation between IV TO IV
 
 
+#####################################
+
+#IV TO DV SCATTER PLOTS
+
+######################################
+
+# correlation between gender and math_score
+plt.scatter(gender, scaled_math_score)
+plt.xlabel('gender')
+plt.ylabel('math_score')
+plt.title('Relationship between Gender and Math Score')
+plt.show()
+plt.scatter(gender, scaled_math_score)
+plt.xlabel('gender')
+plt.ylabel('math_score')
+plt.title('Relationship between Gender and Math Score')
+plt.show()
+
+# correlation between gender and history_score
+plt.scatter(gender, scaled_history_score)
+plt.xlabel('gender')
+plt.ylabel('history_score')
+plt.title('Relationship between Gender and History Score')
+plt.show()
+
+# correlation between gender and physics_score
+plt.scatter(gender, scaled_physics_score)
+plt.xlabel('gender')
+plt.ylabel('physics_score')
+plt.title('Relationship between Gender and Physics Score')
+plt.show()
+
+# correlation between gender and chemistry_score
+plt.scatter(gender, scaled_chemistry_score)
+plt.xlabel('gender')
+plt.ylabel('chemistry_score')
+plt.title('Relationship between Gender and Chemistry Score')
+plt.show()
+
+# correlation between gender and biology_score
+plt.scatter(gender, scaled_biology_score)
+plt.xlabel('gender')
+plt.ylabel('biology_score')
+plt.title('Relationship between Gender and Biology Score')
+plt.show()
+
+# correlation between gender and english_score
+plt.scatter(gender, scaled_english_score)
+plt.xlabel('gender')
+plt.ylabel('english_score')
+plt.title('Relationship between Gender and English Score')
+plt.show()
+
+# correlation between gender and geography_score
+plt.scatter(gender, scaled_geography_score)
+plt.xlabel('gender')
+plt.ylabel('geography_score')
+plt.title('Relationship between Gender and Geography Score')
+plt.show()
+
+# correlation between part_time_job and math_score
+plt.scatter(part_time_job, scaled_math_score)
+plt.xlabel('part_time_job')
+plt.ylabel('math_score')
+plt.title('Relationship between Part-Time Job and Math Score')
+plt.show()
+
+# correlation between part_time_job and history_score
+plt.scatter(part_time_job, scaled_history_score)
+plt.xlabel('part_time_job')
+plt.ylabel('history_score')
+plt.title('Relationship between Part-Time Job and History Score')
+plt.show()
+
+# correlation between part_time_job and physics_score
+plt.scatter(part_time_job, scaled_physics_score)
+plt.xlabel('part_time_job')
+plt.ylabel('physics_score')
+plt.title('Relationship between Part-Time Job and Physics Score')
+plt.show()
+
+# correlation between part_time_job and chemistry_score
+plt.scatter(part_time_job, scaled_chemistry_score)
+plt.xlabel('part_time_job')
+plt.ylabel('chemistry_score')
+plt.title('Relationship between Part-Time Job and Chemistry Score')
+plt.show()
+
+# correlation between part_time_job and biology_score
+plt.scatter(part_time_job, scaled_biology_score)
+plt.xlabel('part_time_job')
+plt.ylabel('biology_score')
+plt.title('Relationship between Part-Time Job and Biology Score')
+plt.show()
+
+# correlation between part_time_job and english_score
+plt.scatter(part_time_job, scaled_english_score)
+plt.xlabel('part_time_job')
+plt.ylabel('english_score')
+plt.title('Relationship between Part-Time Job and English Score')
+plt.show()
+
+# correlation between part_time_job and geography_score
+plt.scatter(part_time_job, scaled_geography_score)
+plt.xlabel('part_time_job')
+plt.ylabel('geography_score')
+plt.title('Relationship between Part-Time Job and Geography Score')
+plt.show()
+
+# correlation between extracurricular_activities and math_score
+plt.scatter(extracurricular_activities, scaled_math_score)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('math_score')
+plt.title('Relationship between Extracurricular Activities and Math Score')
+plt.show()
+
+# correlation between extracurricular_activities and history_score
+plt.scatter(extracurricular_activities, scaled_history_score)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('history_score')
+plt.title('Relationship between Extracurricular Activities and History Score')
+plt.show()
+
+# correlation between extracurricular_activities and physics_score
+plt.scatter(extracurricular_activities, scaled_physics_score)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('physics_score')
+plt.title('Relationship between Extracurricular Activities and Physics Score')
+plt.show()
+
+# correlation between extracurricular_activities and chemistry_score
+plt.scatter(extracurricular_activities, scaled_chemistry_score)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('chemistry_score')
+plt.title('Relationship between Extracurricular Activities and Chemistry Score')
+plt.show()
+
+# correlation between extracurricular_activities and biology_score
+plt.scatter(extracurricular_activities, scaled_biology_score)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('biology_score')
+plt.title('Relationship between Extracurricular Activities and Biology Score')
+plt.show()
+
+# correlation between extracurricular_activities and english_score
+plt.scatter(extracurricular_activities, scaled_english_score)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('english_score')
+plt.title('Relationship between Extracurricular Activities and English Score')
+plt.show()
+
+# correlation between extracurricular_activities and geography_score
+plt.scatter(extracurricular_activities, scaled_geography_score)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('geography_score')
+plt.title('Relationship between Extracurricular Activities and Geography Score')
+plt.show()
+
+
+# correlation between weekly_self_study_hours and math_score
+plt.scatter(scaled_weekly_self_study_hours, scaled_math_score)
+plt.xlabel('weekly_self_study_hours')
+plt.ylabel('math_score')
+plt.title('Relationship between Weekly Self Study Hours and Math Score')
+plt.show()
+
+# correlation between weekly_self_study_hours and history_score
+plt.scatter(scaled_weekly_self_study_hours, scaled_history_score)
+plt.xlabel('weekly_self_study_hours')
+plt.ylabel('history_score')
+plt.title('Relationship between Weekly Self Study Hours and History Score')
+plt.show()
+
+# correlation between weekly_self_study_hours and physics_score
+plt.scatter(scaled_weekly_self_study_hours, scaled_physics_score)
+plt.xlabel('weekly_self_study_hours')
+plt.ylabel('physics_score')
+plt.title('Relationship between Weekly Self Study Hours and Physics Score')
+plt.show()
+
+# correlation between weekly_self_study_hours and chemistry_score
+plt.scatter(scaled_weekly_self_study_hours, scaled_chemistry_score)
+plt.xlabel('weekly_self_study_hours')
+plt.ylabel('chemistry_score')
+plt.title('Relationship between Weekly Self Study Hours and Chemistry Score')
+plt.show()
+
+# correlation between weekly_self_study_hours and biology_score
+plt.scatter(scaled_weekly_self_study_hours, scaled_biology_score)
+plt.xlabel('weekly_self_study_hours')
+plt.ylabel('biology_score')
+plt.title('Relationship between Weekly Self Study Hours and Biology Score')
+plt.show()
+
+# correlation between weekly_self_study_hours and english_score
+plt.scatter(scaled_weekly_self_study_hours, scaled_english_score)
+plt.xlabel('weekly_self_study_hours')
+plt.ylabel('english_score')
+plt.title('Relationship between Weekly Self Study Hours and English Score')
+plt.show()
+
+# correlation between weekly_self_study_hours and geography_score
+plt.scatter(scaled_weekly_self_study_hours, scaled_geography_score)
+plt.xlabel('weekly_self_study_hours')
+plt.ylabel('geography_score')
+plt.title('Relationship between Weekly Self Study Hours and Geography Score')
+plt.show()
+
+#Strong correlation between weekly_self_study_hours and math_score, physics_score, chemistry_score, biology_score, english_score, geography_score
+
+
+
+
 
