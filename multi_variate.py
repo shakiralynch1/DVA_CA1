3d39f9c776995691aa21f89c3a17645edb5bc1dc
shakiralynch1 <d00241906@student.dkit.ie>
2024-04-09 09:54:02 +0100

    :recycle: Adding correlations back in

--- multi_variate.py
+++ multi_variate.py
@@ -45,7 +45,260 @@ scaled_english_score = scaler.fit_transform(english_score.values.reshape(-1, 1))
 scaled_geography_score = scaler.fit_transform(geography_score.values.reshape(-1, 1))
 
 #IV TO IV SCATTER PLOTS
+# IV TO IV SCATTER PLOTS
 
+# correlation between gender and part_time_job
+plt.scatter(missing_days, part_time_job)
+plt.xlabel('missing days')
+plt.ylabel('part_time_job')
+plt.title('Relationship between Gender and Part-Time Job')
+plt.show()
+
+# correlation between gender and extracurricular_activities
+plt.scatter(missing_days, extracurricular_activities)
+plt.xlabel('missing_days')
+plt.ylabel('extracurricular_activities')
+plt.title('Relationship between Gender and Extracurricular Activities')
+plt.show()
+
+# correlation between gender and weekly_self_study_hours
+plt.scatter(missing_days, scaled_weekly_self_study_hours)
+plt.xlabel('missing_days')
+plt.ylabel('weekly_self_study_hours')
+plt.title('Relationship between Gender and Weekly Self Study Hours')
+plt.show()
+
+# correlation between part_time_job and extracurricular_activities
+plt.scatter(part_time_job, extracurricular_activities)
+plt.xlabel('part_time_job')
+plt.ylabel('extracurricular_activities')
+plt.title('Relationship between Part-Time Job and Extracurricular Activities')
+plt.show()
+
+# correlation between part_time_job and weekly_self_study_hours
+plt.scatter(part_time_job, scaled_weekly_self_study_hours)
+plt.xlabel('part_time_job')
+plt.ylabel('weekly_self_study_hours')
+plt.title('Relationship between Part-Time Job and Weekly Self Study Hours')
+plt.show()
+
+# correlation between extracurricular_activities and weekly_self_study_hours
+plt.scatter(extracurricular_activities, scaled_weekly_self_study_hours)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('weekly_self_study_hours')
+plt.title('Relationship between Extracurricular Activities and Weekly Self Study Hours')
+plt.show()
+
+##No correlation between IV TO IV
+
+
+#####################################
+
+#IV TO DV SCATTER PLOTS
+
+######################################
+
+# correlation between gender and math_score
+plt.scatter(missing_days, scaled_math_score)
+plt.xlabel('missing_days')
+plt.ylabel('math_score')
+plt.title('Relationship between Gender and Math Score')
+plt.show()
+plt.scatter(missing_days, scaled_math_score)
+plt.xlabel('missing_days')
+plt.ylabel('math_score')
+plt.title('Relationship between Gender and Math Score')
+plt.show()
+
+# correlation between gender and history_score
+plt.scatter(missing_days, scaled_history_score)
+plt.xlabel('missing_days')
+plt.ylabel('history_score')
+plt.title('Relationship between Gender and History Score')
+plt.show()
+
+# correlation between gender and physics_score
+plt.scatter(missing_days, scaled_physics_score)
+plt.xlabel('missing_days')
+plt.ylabel('physics_score')
+plt.title('Relationship between Gender and Physics Score')
+plt.show()
+
+# correlation between gender and chemistry_score
+plt.scatter(missing_days, scaled_chemistry_score)
+plt.xlabel('missing_days')
+plt.ylabel('chemistry_score')
+plt.title('Relationship between Gender and Chemistry Score')
+plt.show()
+
+# correlation between gender and biology_score
+plt.scatter(missing_days, scaled_biology_score)
+plt.xlabel('missing_days')
+plt.ylabel('biology_score')
+plt.title('Relationship between Gender and Biology Score')
+plt.show()
+
+# correlation between gender and english_score
+plt.scatter(missing_days, scaled_english_score)
+plt.xlabel('missing_days')
+plt.ylabel('english_score')
+plt.title('Relationship between Gender and English Score')
+plt.show()
+
+# correlation between gender and geography_score
+plt.scatter(missing_days, scaled_geography_score)
+plt.xlabel('missing_days')
+plt.ylabel('geography_score')
+plt.title('Relationship between Gender and Geography Score')
+plt.show()
+
+# correlation between part_time_job and math_score
+plt.scatter(part_time_job, scaled_math_score)
+plt.xlabel('part_time_job')
+plt.ylabel('math_score')
+plt.title('Relationship between Part-Time Job and Math Score')
+plt.show()
+
+# correlation between part_time_job and history_score
+plt.scatter(part_time_job, scaled_history_score)
+plt.xlabel('part_time_job')
+plt.ylabel('history_score')
+plt.title('Relationship between Part-Time Job and History Score')
+plt.show()
+
+# correlation between part_time_job and physics_score
+plt.scatter(part_time_job, scaled_physics_score)
+plt.xlabel('part_time_job')
+plt.ylabel('physics_score')
+plt.title('Relationship between Part-Time Job and Physics Score')
+plt.show()
+
+# correlation between part_time_job and chemistry_score
+plt.scatter(part_time_job, scaled_chemistry_score)
+plt.xlabel('part_time_job')
+plt.ylabel('chemistry_score')
+plt.title('Relationship between Part-Time Job and Chemistry Score')
+plt.show()
+
+# correlation between part_time_job and biology_score
+plt.scatter(part_time_job, scaled_biology_score)
+plt.xlabel('part_time_job')
+plt.ylabel('biology_score')
+plt.title('Relationship between Part-Time Job and Biology Score')
+plt.show()
+
+# correlation between part_time_job and english_score
+plt.scatter(part_time_job, scaled_english_score)
+plt.xlabel('part_time_job')
+plt.ylabel('english_score')
+plt.title('Relationship between Part-Time Job and English Score')
+plt.show()
+
+# correlation between part_time_job and geography_score
+plt.scatter(part_time_job, scaled_geography_score)
+plt.xlabel('part_time_job')
+plt.ylabel('geography_score')
+plt.title('Relationship between Part-Time Job and Geography Score')
+plt.show()
+
+# correlation between extracurricular_activities and math_score
+plt.scatter(extracurricular_activities, scaled_math_score)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('math_score')
+plt.title('Relationship between Extracurricular Activities and Math Score')
+plt.show()
+
+# correlation between extracurricular_activities and history_score
+plt.scatter(extracurricular_activities, scaled_history_score)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('history_score')
+plt.title('Relationship between Extracurricular Activities and History Score')
+plt.show()
+
+# correlation between extracurricular_activities and physics_score
+plt.scatter(extracurricular_activities, scaled_physics_score)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('physics_score')
+plt.title('Relationship between Extracurricular Activities and Physics Score')
+plt.show()
+
+# correlation between extracurricular_activities and chemistry_score
+plt.scatter(extracurricular_activities, scaled_chemistry_score)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('chemistry_score')
+plt.title('Relationship between Extracurricular Activities and Chemistry Score')
+plt.show()
+
+# correlation between extracurricular_activities and biology_score
+plt.scatter(extracurricular_activities, scaled_biology_score)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('biology_score')
+plt.title('Relationship between Extracurricular Activities and Biology Score')
+plt.show()
+
+# correlation between extracurricular_activities and english_score
+plt.scatter(extracurricular_activities, scaled_english_score)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('english_score')
+plt.title('Relationship between Extracurricular Activities and English Score')
+plt.show()
+
+# correlation between extracurricular_activities and geography_score
+plt.scatter(extracurricular_activities, scaled_geography_score)
+plt.xlabel('extracurricular_activities')
+plt.ylabel('geography_score')
+plt.title('Relationship between Extracurricular Activities and Geography Score')
+plt.show()
+
+
+# correlation between weekly_self_study_hours and math_score
+plt.scatter(scaled_weekly_self_study_hours, scaled_math_score)
+plt.xlabel('weekly_self_study_hours')
+plt.ylabel('math_score')
+plt.title('Relationship between Weekly Self Study Hours and Math Score')
+plt.show()
+
+# correlation between weekly_self_study_hours and history_score
+plt.scatter(scaled_weekly_self_study_hours, scaled_history_score)
+plt.xlabel('weekly_self_study_hours')
+plt.ylabel('history_score')
+plt.title('Relationship between Weekly Self Study Hours and History Score')
+plt.show()
+
+# correlation between weekly_self_study_hours and physics_score
+plt.scatter(scaled_weekly_self_study_hours, scaled_physics_score)
+plt.xlabel('weekly_self_study_hours')
+plt.ylabel('physics_score')
+plt.title('Relationship between Weekly Self Study Hours and Physics Score')
+plt.show()
+
+# correlation between weekly_self_study_hours and chemistry_score
+plt.scatter(scaled_weekly_self_study_hours, scaled_chemistry_score)
+plt.xlabel('weekly_self_study_hours')
+plt.ylabel('chemistry_score')
+plt.title('Relationship between Weekly Self Study Hours and Chemistry Score')
+plt.show()
+
+# correlation between weekly_self_study_hours and biology_score
+plt.scatter(scaled_weekly_self_study_hours, scaled_biology_score)
+plt.xlabel('weekly_self_study_hours')
+plt.ylabel('biology_score')
+plt.title('Relationship between Weekly Self Study Hours and Biology Score')
+plt.show()
+
+# correlation between weekly_self_study_hours and english_score
+plt.scatter(scaled_weekly_self_study_hours, scaled_english_score)
+plt.xlabel('weekly_self_study_hours')
+plt.ylabel('english_score')
+plt.title('Relationship between Weekly Self Study Hours and English Score')
+plt.show()
+
+# correlation between weekly_self_study_hours and geography_score
+plt.scatter(scaled_weekly_self_study_hours, scaled_geography_score)
+plt.xlabel('weekly_self_study_hours')
+plt.ylabel('geography_score')
+plt.title('Relationship between Weekly Self Study Hours and Geography Score')
+plt.show()
 
 
 #Strong correlation between weekly_self_study_hours and math_score, physics_score, chemistry_score, biology_score, english_score, geography_score
@@ -177,6 +430,14 @@ print(f"Pearson correlation between weekly_self_study_hours and geography_score:
 #Pearson correlation between weekly_self_study_hours and english_score: 0.2477960153508276
 #Pearson correlation between weekly_self_study_hours and geography_score: 0.15362244292021798
 
+
+
+all_data = sm.add_constant(all_data)
+model = sm.OLS(prices, all_data)
+result = model.fit()
+print(result.summary())
+# Line below is for the standard error
+print(result.scale**0.5)
 
 
 
