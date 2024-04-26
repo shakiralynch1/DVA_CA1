44e397daeb3645bfdd9ea91f97c220081f51158e
shakiralynch1 <d00241906@student.dkit.ie>
2024-04-26 19:45:27 +0100

    :broom: cleaned csv more

--- main.py
+++ main.py
@@ -9,16 +9,22 @@ df = pd.read_csv('student-scores.csv')
 print(df.head())
 
 # One-hot encode the data 
-encoded_df = pd.get_dummies(df, columns=['part_time_job', 'extracurricular_activities'], prefix=['part_time_job', 'extracurricular_activities'], dtype=int, )
-print(encoded_df.head())
+
 
 # Drop the columns that are not needed
-df = pd.DataFrame(encoded_df)
-obselete_columns = ['id','first_name','last_name','email', 'career_aspiration', 'part_time_job_False', 'extracurricular_activities_False']
+df = pd.DataFrame(df)
+obselete_columns = ['id','first_name','last_name','email','gender', 'career_aspiration']
 df.drop(obselete_columns, axis=1, inplace=True)
-
 print(df.head())
 
+df = pd.get_dummies(df, columns=['part_time_job', 'extracurricular_activities'], prefix=['part_time_job', 'extracurricular_activities'], dtype=int, )
+print(df.head())
+
+obselete_columns = ['part_time_job_False','extracurricular_activities_False']
+df.drop(obselete_columns, axis=1, inplace=True)
+
+
+print(df)
 # Save the modified data
 df.to_csv('modified_student_scores.csv', index=False)
 
